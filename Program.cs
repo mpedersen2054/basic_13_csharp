c5dc6b1b0bb04616879fdbb7e39a3b40ff798023
Matt Pedersen <mattpp@Matts-MacBook-Air.local>
2017-02-07 11:32:09 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void printAllNums()
+        {
+            for (int i = 1; i < 256; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void printOddNums()
+        {
+            for (int i = 1; i < 256; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        public static void printSum()
+        {
+            int sum = 0;
+            for (int i = 0; i < 256; i++)
+            {
+                sum += i;
+                Console.WriteLine("New number: {0} | Sum: {1}", i, sum);
+            }
+            // Console.WriteLine(sum);
+        }
+        public static void printArrVals()
+        {
+            Console.WriteLine("hello");
+        }
+        public static void Main(string[] args)
+        {
+            // printAllNums();
+            // printOddNums();
+            // printSum();
+            printArrVals();
+        }
+    }
+}
